69826e16846e62fab9b534ba4e68429dbaf39205
serranocedeno3 <serranocedeno3@gmail.com>
2025-09-17 16:05:53 -0400

    Create Answer 1.6 App.jsx
    answer fullstackopen 1.6

---
+++ part1/Answer 1.6 App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+
+
+
+
+
+const Button=(props)=>{
+
+  return(
+    <div>
+
+<button onClick={props.handlerClick}>{props.name}</button>
+    </div>
+  )
+}
+
+
+const App = () => {
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  
+
+
+
+  return (
+    <div>
+      <h1>give feedback</h1>
+
+<div style={{display:'flex', gap:'1%'}}>
+<Button name='good' handlerClick={()=>setGood(good+1)}  />
+<Button name='neutral' handlerClick={()=>setNeutral(neutral+1)} />
+<Button name='bad' handlerClick={()=>setBad(bad+1)} />
+</div>
+
+<br />
+
+<h1>statistics</h1>
+<br />
+<p>good {good}</p>
+<p>neutral  {neutral}</p>
+<p>bad  {bad}</p>
+    </div>
+  )
+}
+
+export default App   
