ccf3a755609c492b41b8abd8a4738dc53ff4511b
serranocedeno3 <serranocedeno3@gmail.com>
2025-09-20 15:57:49 +0200

    Update App.jsx

--- CodigoWeb/App.jsx
+++ CodigoWeb/App.jsx
@@ -1,66 +1,23 @@
- import { useState } from "react";
-import Course from "./components/Course";
-
- 
+import { useState } from 'react'
+import Note from './components/Note'
 
 
+const App = (props) => {
+  const [notes, setNotes] = useState(props.notes)
 
-const App = () => {
-
-
-  const course =[{
-    id: 1,
-    name: 'Half Stack application development',
-    parts: [
-      {
-        name: 'Fundamentals of React',
-        exercises: 10,
-        id: 1
-      },
-      {
-        name: 'Using props to pass data',
-        exercises: 7,
-        id: 2
-      },
-      {
-        name: 'State of a component',
-        exercises: 14,
-        id: 3
-      },
-       {
-        name: 'Redux',
-        exercises: 11,
-        id: 4
-      }
-    ]
-  },
-
-{
-  name: 'Node.js',
-      id: 2,
-      parts: [
-        {
-          name: 'Routing',
-          exercises: 3,
-          id: 1
-        },
-        {
-          name: 'Middlewares',
-          exercises: 7,
-          id: 2
-        }
-      ]
+  return (
+    <div>
+      <h1>Notes</h1>
+      <ul>
+        {notes.map(note => 
+          <Note key={note.id} note={note} />
+        )}
+      </ul>
+    </div>
+  )
 }
 
-] 
-
-
-
-  return <Course course={course} />
-}
-
-
-export default App
+export default App 
 
 
 
