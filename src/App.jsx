919749f6d9d0acd9260273fa7cd4ce7deb94554f
serranocedeno3 <serranocedeno3@gmail.com>
2025-09-21 23:10:01 +0200

    Aplicacion de la agenda desde 2.6 hasta la 2.11

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,25 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import Filter from './components/Filter'
 import Form from './components/Form'
 import Person from './components/Person'
+import axios from 'axios'
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [resultFilter, setResultFilter] = useState('')
+
+
+  useEffect(() => {
+    console.log('effect')
+    axios.get('http://localhost:3001/persons').then((response) => {
+      console.log('promise fulfilled')
+          
+      setPersons(response.data)
+    })
+  }, [])
+
 
 const handlerChangeName=(event)=>{
 setNewName(event.target.value)
@@ -46,13 +53,14 @@ if (existe) {
    alert(`${newName} is already added to phonebook`)
 } else {
  setPersons(persons.concat(personsObject))
+ setResultFilter(persons.concat(personsObject))
+ 
  
 
 }
 setNewName('')
 setNewNumber('')
 }
-
 
   return (
     <div>
